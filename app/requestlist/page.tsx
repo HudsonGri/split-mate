4a66d97bcba69d63ad3b8475cc79bb503818a2f1
scarpio02 <scarpio@ufl.edu>
2024-04-03 12:35:13 -0400

    implemented request list page

--- app/requestlist/page.tsx
+++ app/requestlist/page.tsx
@@ -1,53 +1,25 @@
-import { Metadata } from "next"
-import Image from "next/image"
-
+import { Request, columns } from "./columns"
 import { Button } from "../../components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "../../components/ui/card"
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "../../components/ui/tabs"
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-  TableFooter,
-} from "../../components/ui/table"
-import { CalendarDateRangePicker } from "../../components/date-range-picker"
-import { MainNav } from "../../components/main-nav"
-import { Overview } from "../../components/overview"
-import { RecentSales } from "../../components/recent-sales"
-import { Search } from "../../components/search"
-import TeamSwitcher from "../../components/team-switcher"
-import { UserNav } from "../../components/user-nav"
+import { DataTable } from "../../components/ui/data-table"
 import { NavBar } from "@/components/nav"
-import { PlusIcon } from "@radix-ui/react-icons"
-import Link from 'next/link'
-import { buttonVariants } from "@/components/ui/button"
 import { createClient } from '@/utils/supabase/server'
-import { link } from "fs"
 import { redirect } from 'next/navigation'
 
-
-export const metadata: Metadata = {
-  title: "Request List",
-  description: "Example dashboard app built using the components.",
+async function getData(): Promise<Request[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      name: "Paper Towels",
+      user_submitted: "John B.",
+      status: "pending approval",
+    },
+    // ...
+  ]
 }
 
-export default async function DashboardPage() {
+export default async function RequestListPage() {
+  const requestdata = await getData()
 
   const supabase = createClient()
 
@@ -58,8 +30,8 @@ export default async function DashboardPage() {
     redirect('/')
   }
   const user = data.user
-  
-  
+
+    
   return (
     <>
       <div className="flex flex-col">
@@ -75,175 +47,12 @@ export default async function DashboardPage() {
               <Button>test</Button>
             </div>
           </div>
-          <Tabs defaultValue="overview" className="space-y-4">
-            <TabsList>
-              <TabsTrigger value="overview">Overview</TabsTrigger>
-              <TabsTrigger value="history">
-                History
-              </TabsTrigger>
-              <TabsTrigger value="people">
-                People
-              </TabsTrigger>
-              <TabsTrigger value="notifications" disabled>
-                Notifications
-              </TabsTrigger>
-            </TabsList>
-            <TabsContent value="overview" className="space-y-4">
-              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">
-                      Total Revenue
-                    </CardTitle>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                    </svg>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">$45,231.89</div>
-                    <p className="text-xs text-muted-foreground">
-                      +20.1% from last month
-                    </p>
-                  </CardContent>
-                </Card>
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">
-                      Subscriptions
-                    </CardTitle>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                      <circle cx="9" cy="7" r="4" />
-                      <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                    </svg>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">+2350</div>
-                    <p className="text-xs text-muted-foreground">
-                      +180.1% from last month
-                    </p>
-                  </CardContent>
-                </Card>
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">Sales</CardTitle>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <rect width="20" height="14" x="2" y="5" rx="2" />
-                      <path d="M2 10h20" />
-                    </svg>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">+12,234</div>
-                    <p className="text-xs text-muted-foreground">
-                      +19% from last month
-                    </p>
-                  </CardContent>
-                </Card>
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">
-                      Active Now
-                    </CardTitle>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                    </svg>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">+573</div>
-                    <p className="text-xs text-muted-foreground">
-                      +201 since last hour
-                    </p>
-                  </CardContent>
-                </Card>
-              </div>
-              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-                <Card className="col-span-4">
-                  <CardHeader>
-                    <CardTitle>Overview</CardTitle>
-                  </CardHeader>
-                  <CardContent className="pl-2">
-                    <Overview />
-                  </CardContent>
-                </Card>
-                <Card className="col-span-3">
-                  <CardHeader>
-                    <CardTitle>Request List</CardTitle>
-                    <CardDescription>
-                      You made 265 sales this month.
-                    </CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <RecentSales />
-                  </CardContent>
-                  <CardFooter className="justify-center">
-                  <Button variant="outline">Request Item</Button>
-                  </CardFooter>
-                </Card>
-              </div>
-            </TabsContent>
-            <TabsContent value="people" className="space-y-4">
-            <Table>
-              <TableCaption>A list of people in this group.</TableCaption>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="w-[200px]">Name</TableHead>
-                  <TableHead>Email</TableHead>
-                  <TableHead className="text-right">Phone Number</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                <TableRow>
-                  <TableCell className="font-medium">Albert Gator</TableCell>
-                  <TableCell>algator@ufl.edu</TableCell>
-                  <TableCell className="text-right">123-456-7890</TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell className="font-medium">Alberta Gator</TableCell>
-                  <TableCell>alberta.gator@ufl.edu</TableCell>
-                  <TableCell className="text-right">111-444-7777</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-            </TabsContent>
-          </Tabs>
         </div>
       </div>
+    
+        <div className="container mx-auto py-10">
+            <DataTable columns={columns} data={requestdata} />
+        </div>
     </>
   )
 }
