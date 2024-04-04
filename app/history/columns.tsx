ed1446a2cfa2b2f3f34a6ae24ad0b63dfbc44be9
Claudio Sciotto <fisizion@gmail.com>
2024-04-04 13:10:28 -0400

    Started working on history page
    Broken not functional yet

---
+++ app/history/columns.tsx
@@ -0,0 +1,76 @@
+// columns.tsx
+import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { Checkbox } from "../../components/ui/checkbox"
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "../../components/ui/dropdown-menu"
+
+export type Request = {
+    id: string
+    date: string
+    amount: string
+    sender: string
+    expense: string
+    typeOfAction: "Payment" | "Requested item" // Add more if needed
+    receiver: string
+}
+
+export const columns: ColumnDef<Request>[] = [
+    {
+        id: "actions",
+        header: "Actions",
+        cell: ({ row }) => {
+            const request = row.original
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem onClick={() => navigator.clipboard.writeText(request.id)}>
+                            Copy payment ID
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>View requestor</DropdownMenuItem>
+                        <DropdownMenuItem>View request details</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        },
+    },
+    {
+        accessorKey: "date",
+        header: "Date",
+    },
+    {
+        accessorKey: "amount",
+        header: "Amount",
+    },
+    {
+        accessorKey: "sender",
+        header: "Sender",
+    },
+    {
+        accessorKey: "receiver",
+        header: "Receiver",
+    },
+    {
+        accessorKey: "expense",
+        header: "Expense",
+    },
+    {
+        accessorKey: "typeOfAction",
+        header: "Type of Action",
+    },
+]
