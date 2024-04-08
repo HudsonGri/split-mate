c7878c847303cf6d1e7174f92118dc9d402e3256
Hudson <ardroneheart@gmail.com>
2024-04-08 15:28:49 -0400

    Redirect to login upon signin and showing name

--- components/user-nav.tsx
+++ components/user-nav.tsx
@@ -21,15 +21,14 @@ export function UserNav({ user_details }: { user_details?: any }) {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" />
-            <AvatarFallback>EX</AvatarFallback>
+            <AvatarFallback>{user_details.user_metadata.full_name.slice(0,2).toUpperCase()}</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">Name</p>
+            <p className="text-sm font-medium leading-none">{user_details.user_metadata.full_name}</p>
             <p className="text-xs leading-none text-muted-foreground">
               {user_details.email}
             </p>
