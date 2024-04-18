27ca606c6bee90fbbf71f6f6a004ca1e8cae34df
Claudio Sciotto <claudiogsdb@gmail.com>
2024-04-18 18:39:32 -0400

    transfer

--- app/dashboard/history/history.tsx
+++ app/dashboard/history/history.tsx
@@ -7,8 +7,18 @@ import { createClient } from '@/utils/supabase/server'
 import { redirect } from 'next/navigation'
 
 async function getData(): Promise<Request[]> {
+
+  
   // Fetch data from your API here.
   const supabase = createClient()
+  
+  const { data, error: error1 } = await supabase.auth.getUser()
+  console.log(error1)
+  if (error1 || !data?.user) {
+    console.log("redirect to main")
+    redirect('/')
+  }
+  const user = data.user;
 
   let { data: expenses, error } = await supabase
   .from('expenses')
@@ -16,22 +26,43 @@ async function getData(): Promise<Request[]> {
 
   if (error) {
     console.log("redirect to main")
-    redirect('/')
-  }
+    redirect('/dashboard')
+  } 
+  
+  // let { data: myGroup, error: error1 } = await supabase
+  // .from('groups')
+  // .select('*')
+  // .eq('name', 'test');
+
+  
+  // let { data: expenses, error: error2 } = await supabase
+  // .from('users')
+  // .select(`
+  //   *,
+  //   groups (
+  //     group_id
+  //   )
+  // `)
+  // .eq('groups.group_id', myGroup);
+
+  // if (error1 || error2) {
+  //   console.log("redirect to main")
+  //   redirect('/dashboard')
+  // } 
 
 
+
+  
   if (expenses) {
-    console.log("got here");
     const requests: Request[] = expenses.map((item: any) => ({
       id: item.id,
-      date: new Date(item.date),
-      amount: item.amount,
-      payer: item.payer_id,
-      expense: item.expense,
-      typeOfAction: item.typeOfAction,
-      receiver: item.receiver,
+      date: new Date(item.creation_date), //rdy
+      amount: item.amount, //rdy
+      payer: user?.full_name, //rdy
+      expense: item.description, //rdy
+      typeOfAction: item.testingshit, //NOT IN TABLE
+      receiver: item.receiver, //NOT IN TABLE
     }));
-
     return requests;
   }
   
@@ -50,15 +81,20 @@ export default async function History() {
     console.log("redirect to main")
     redirect('/')
   }
-  const user = data.user
+  const user = data.user;
 
   return (
     <>
+      <div>
+        {/* <h1><pre>{JSON.stringify(data.user.user_metadata.full_name, null, 2)}</pre></h1> */}
+        <h1><pre>{JSON.stringify(user, null, 2)}</pre></h1>
+      </div>
       <div className="container mx-auto">
         <DataTable columns={columns} data={requestdata} />
       </div>
+      
     </>
-  )
+  );
 }
 
 export { History };
