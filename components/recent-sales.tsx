3f96c9ade7cd3913f98cf5f3df7f876a7fa6fbc0
Hudson <ardroneheart@gmail.com>
2024-04-30 19:59:38 -0400

    request list testing

--- components/recent-sales.tsx
+++ components/recent-sales.tsx
@@ -1,54 +1,48 @@
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "./ui/avatar"
+"use client";
+import { useEffect, useState } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
 
 export function RecentSales() {
+  const [requests, setRequests] = useState([]);
+  const groupId = "7146eef7-5f38-4113-a212-80ee31b63b8a"; // Hardcoded group_id for testing
 
-  const requests = [
-    {
-      id: 1,
-      name: "Paper Towels",
-      cost: "5.00",
-      user_submitted: "Josh B.",
-    },
-    {
-      id: 1,
-      name: "Paper Towels",
-      cost: "25.00",
-      user_submitted: "Joe B.",
-    },
-    {
-      id: 1,
-      name: "Paper Towels",
-      cost: "13.00",
-      user_submitted: "Josh B.",
-    },
-    {
-      id: 1,
-      name: "Paper Towels",
-      cost: "1.00",
-      user_submitted: "Josh B.",
-    },
-  ];
+  useEffect(() => {
+    fetch(`/api/requests`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ group_id: groupId }),
+    })
+      .then((response) => response.json())
+      .then((data) => setRequests(data))
+      .catch((error) => console.error("Error fetching data: ", error));
+  }, [groupId]);
+
   return (
     <div className="space-y-8">
       {requests.map((item, index) => (
         <div className="flex items-center" key={index}>
           <Avatar className="h-9 w-9">
-            <AvatarImage src="/avatars/01.png" alt="Avatar" />
-            <AvatarFallback>JB</AvatarFallback>
+            <AvatarFallback>
+              {item.creator.first_name.slice(0, 1).toUpperCase()}
+              {item.creator.last_name.slice(0, 1).toUpperCase()}
+            </AvatarFallback>
           </Avatar>
           <div className="ml-4 space-y-1">
-            <p className="text-sm font-medium leading-none">{item.name}</p>
+            <p className="text-sm font-medium leading-none">
+              {item.name} <Badge variant="outline">Badge</Badge>
+            </p>
             <p className="text-sm text-muted-foreground">
-              - {item.user_submitted}
+              - {item.creator.first_name} {item.creator.last_name}
+              {", "}
+              {new Date(item.creation_date).toLocaleDateString("en-US")}
             </p>
           </div>
           <div className="ml-auto font-medium">${item.cost}</div>
         </div>
       ))}
     </div>
-  )
+  );
 }
