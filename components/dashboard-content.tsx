b4ede6070416a6a35052fd157627bcb390ed2b2b
scarpio02 <scarpio@ufl.edu>
2024-05-02 00:39:24 -0400

    Conflict fix

--- components/dashboard-content.tsx
+++ components/dashboard-content.tsx
@@ -159,7 +159,6 @@ export function DashboardContent() {
           <History groupId={currentGroup} />
         </TabsContent>
         <TabsContent value="people" className="space-y-4">
-          {/* <PeopleTable groupId={currentGroup} /> */}
         </TabsContent>
       </Tabs>
     </div>
